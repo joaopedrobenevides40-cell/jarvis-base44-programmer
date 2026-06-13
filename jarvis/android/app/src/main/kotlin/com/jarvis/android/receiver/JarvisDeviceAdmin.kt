932f2685377361e18d740f1f88813f02a5062ee2
joaopedrobenevides40-cell <joaopedrobenevides40@gmail.com>
2026-06-13 17:15:18 -0300

    feat: add jarvis/android/app/src/main/kotlin/com/jarvis/android/receiver/JarvisDeviceAdmin.kt

---
+++ jarvis/android/app/src/main/kotlin/com/jarvis/android/receiver/JarvisDeviceAdmin.kt
@@ -0,0 +1,14 @@
+package com.jarvis.android.receiver
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+
+class JarvisDeviceAdmin : DeviceAdminReceiver() {
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
+    }
+    override fun onDisabled(context: Context, intent: Intent) {
+        super.onDisabled(context, intent)
+    }
+}
